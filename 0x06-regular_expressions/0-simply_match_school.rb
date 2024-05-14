28370d3c08f75ea14e36dbc911d05150dd2b6c3f
g3rry9898 <hydrogenbond36@gmail.com>
2024-05-14 14:00:54 -0700

    regular expression

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,12 @@
+def match_school(input)
+  pattern = /School/
+  if input.match(pattern)
+    puts "Match found!"
+  else
+    puts "No match."
+  end
+end
+
+input = ARGV[0]
+match_school(input) if input
+
